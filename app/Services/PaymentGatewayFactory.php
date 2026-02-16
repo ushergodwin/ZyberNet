f5654aa786b258b3925b393dc4ff2d64c73cbbbe
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2026-02-16 17:03:31 +0300

    Update PaymentGatewayFactory.php
    - update

--- app/Services/PaymentGatewayFactory.php
+++ app/Services/PaymentGatewayFactory.php
@@ -135,8 +135,8 @@ class PaymentGatewayFactory
     {
         $hour = (int) now()->format('H');
 
-        // Day: 9 AM (09:00) to before 11 PM (23:00)
-        if ($hour >= 9 && $hour < 23) {
+        // Day: 9 AM (09:00) to before 10 PM (22:00)
+        if ($hour >= 9 && $hour < 22) {
             return 'yopayments';
         }
 
